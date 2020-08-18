fb42c406281347a3aa686a1faa097aa18c136bd2
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-18 15:44:15 +0100

    Adds test and implementation for default redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    redirect_to "https://podcastplayerofchoice.com"
+  end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
+  describe "#index" do 
+    it "redirects_to https://podcastplayerofchoice.com" do
+      expect(get :index).to redirect_to("https://podcastplayerofchoice.com")
+    end
+  end
 end
