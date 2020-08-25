4870ea0313e7d8f79618f6b5e883dce9c1763ca2
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-26 00:35:48 +0100

    Adds implementation to return 200

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,22 @@
 class FeedsController < ApplicationController
   def create
+    render json: body
+  end
+
+  protected
+
+  def body
+    {
+      request: {
+        feed:  profile_params.fetch(:feed)
+      },
+      # itunes: profile_params.fetch(:feed)
+    }
+  end
+
+  def profile_params
+    params
+      .require(:data)
+      .permit(:feed)
   end
 end
