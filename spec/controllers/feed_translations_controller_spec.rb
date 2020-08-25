64ff9ed74d75b36c8ba76327b4f0c8df3c46d05e
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-25 17:19:01 +0100

    Fixes test and implementation for FeedTranslationsController

--- spec/controllers/feed_translations_controller_spec.rb
+++ spec/controllers/feed_translations_controller_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe FeedTranslationsController, type: :controller do
 
   describe "GET #feeds" do
+    let(:request_body) { 
+      { "data": { "feed": "http://feeds.frackulous.com/frackulous/sd/" } } 
+    }
+
     it "returns http success" do
-      get :feeds
+      post :feeds, :params => request_body
       expect(response).to have_http_status(:success)
     end
   end
