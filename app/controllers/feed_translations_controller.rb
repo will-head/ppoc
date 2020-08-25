79cec7b2d6ba36c670927a33a58779e096c0fac4
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-25 19:03:31 +0100

    Fixes controller implementation

--- app/controllers/feed_translations_controller.rb
+++ app/controllers/feed_translations_controller.rb
@@ -1,4 +1,27 @@
 class FeedTranslationsController < ApplicationController
   def feeds
+    render json: response_body
+  end
+
+  protected
+
+  def calculated_age
+    # AgeCalculator.new(profile_params.fetch(:birthdate)).get_age!
+    30
+  end
+
+  def response_body
+    {
+      request: {
+        feed:  profile_params.fetch(:feed)
+      },
+      itunes: calculated_age
+    }
+  end
+
+  def profile_params
+    params
+      .require(:data)
+      .permit(:feed)
   end
 end
