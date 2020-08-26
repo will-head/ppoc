3ad773b858bb7adf97f634de722977c5b7be11c7
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-26 17:04:51 +0100

    Refactors tests

--- spec/controllers/api/v1/feeds_controller_spec.rb
+++ spec/controllers/api/v1/feeds_controller_spec.rb
@@ -5,14 +5,16 @@ RSpec.describe Api::V1::FeedsController, type: :controller do
     let(:request_body) { 
       { data: { feed: "http://feeds.frackulous.com/frackulous/sd/" } } 
     }
-   
-    it "returns http ok" do
+
+    before(:each) do
       post :create, :params => request_body
+    end
+
+    it "returns http ok" do
       expect(response).to have_http_status(:ok)
     end
 
     it "has a request, desktop, ios and android keys" do
-      post :create, :params => request_body
       expect(JSON.parse(response.body).keys).to contain_exactly("request", "desktop", "ios", "android")
     end
   end
