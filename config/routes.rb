c6464014b582d51a93c2b007ced0c2aec126529b
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-26 13:52:25 +0100

    Refactors routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
+  current_api_routes = lambda do
+    resources :feeds, only: [:create]
+  end
+
   namespace :api do
-    namespace :v1 do
-      resources :feeds, only: [:create]
-    end
+    namespace :v1, &current_api_routes
   end
 end
