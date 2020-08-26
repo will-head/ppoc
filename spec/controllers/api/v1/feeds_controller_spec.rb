6af8a771da2d70ad5d40ea83434b8c17a4bfd927
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-26 16:24:52 +0100

    Refactors tests

--- spec/controllers/api/v1/feeds_controller_spec.rb
+++ spec/controllers/api/v1/feeds_controller_spec.rb
@@ -10,10 +10,5 @@ RSpec.describe Api::V1::FeedsController, type: :controller do
       post :create, :params => request_body
       expect(response).to have_http_status(:ok)
     end
-
-    it "non-version returns latest api" do
-      # post '/api/feeds', :params => request_body
-      expect(:post => "/api/feeds", :params => request_body).to route_to("api/v1/feeds#create")
-    end
   end
 end
--- spec/requests/api_request_spec.rb
+++ spec/requests/api_request_spec.rb
@@ -1,14 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "non-version api", :type => :request do
+  let(:latest_version) { "V1" }
   let(:request_body) { 
     { data: { feed: "http://feeds.frackulous.com/frackulous/sd/" } } 
   }
 
   it "redirects to latest version" do
     post "/api/feeds", :params => request_body
-    # expect(response).to have_http_status(:ok)
-    puts response.headers
-    expect(response.headers["Version"]).to eq("V1")
+    expect(response.headers["Version"]).to eq(latest_version)
   end
 end
