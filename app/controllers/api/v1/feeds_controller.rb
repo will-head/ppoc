e9de01342c8677e13148e1533b442b4a46396d2d
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-26 21:42:18 +0100

    Adds tests and basic implementation for desktop feeds

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,6 +1,10 @@
+require 'feed_translator'
+
 module Api::V1
   class FeedsController < ApiController
+
     def create
+      @feed = FeedTranslator.new(profile_params.fetch(:feed))
       render json: body
     end
 
@@ -11,7 +15,20 @@ module Api::V1
         request: {
           feed:  profile_params.fetch(:feed)
         },
-        desktop: {},
+        desktop: {
+          rss: {
+            feed_title: "RSS",
+            feed_url: @feed.rss
+          },
+          itunes: {
+            feed_title: "iTunes",
+            feed_url: "itpc://example.com/feed"
+          },
+          apple_podcasts: {
+            feed_title: "Apple Podcasts",
+            feed_url: "podcast://example.com/feed"
+          }
+        },
         ios: {},
         android: {}
       }
--- spec/controllers/api/v1/feeds_controller_spec.rb
+++ spec/controllers/api/v1/feeds_controller_spec.rb
@@ -15,8 +15,49 @@ RSpec.describe Api::V1::FeedsController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
 
-    it "has a request, desktop, ios and android keys" do
+    it "has request, desktop, ios and android keys" do
       expect(json_body.keys).to contain_exactly("request", "desktop", "ios", "android")
     end
+
+    it "request has a feed key" do
+      expect(json_body["request"].keys).to contain_exactly("feed")
+    end
+
+    it "request feed value is http://example.com/feed" do
+      expect(json_body["request"]["feed"]).to eq("http://example.com/feed")
+    end
+
+    it "desktop key has rss, itunes and apple_podcasts keys" do
+      expect(json_body["desktop"].keys).to contain_exactly("rss", "itunes", "apple_podcasts")
+    end
+
+    it "desktop rss key has feed_title and feed_url keys" do
+      expect(json_body["desktop"]["rss"].keys).to contain_exactly("feed_title", "feed_url")
+    end
+
+    it "desktop rss feed_title value is RSS" do
+      expect(json_body["desktop"]["rss"]["feed_title"]).to eq("RSS")
+    end
+
+    it "desktop rss feed_url value is http://example.com/feed" do
+      expect(json_body["desktop"]["rss"]["feed_url"]).to eq("http://example.com/feed")
+    end
+
+    it "desktop itunes feed_title value is iTunes" do
+      expect(json_body["desktop"]["itunes"]["feed_title"]).to eq("iTunes")
+    end
+
+    it "desktop itunes feed_url value is itpc://example.com/feed" do
+      expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed")
+    end
+
+    it "desktop apple_podcasts feed_title value is Apple Podcasts" do
+      expect(json_body["desktop"]["apple_podcasts"]["feed_title"]).to eq("Apple Podcasts")
+    end
+
+    it "desktop apple_podcasts feed_url value is podcast://example.com/feed" do
+      expect(json_body["desktop"]["apple_podcasts"]["feed_url"]).to eq("podcast://example.com/feed")
+    end
+
   end
 end
