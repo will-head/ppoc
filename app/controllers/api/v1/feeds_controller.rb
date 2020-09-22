8a91032adde1e68dfd4ecf09a6df3e74fac5ca1c
@willhead <60652390+will-head@users.noreply.github.com>
2020-09-22 12:41:13 +0100

    Removes commented code

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -10,66 +10,14 @@ module Api::V1
 
     protected
 
-    # rubocop:disable Metrics/MethodLength
     def body
       {
         request: { feed: profile_params.fetch(:feed) },
         desktop: @feed.desktop,
         ios: @feed.ios,
         android: @feed.android
-        # desktop: {
-        #   default: {
-        #     feed_title: "Default",
-        #     feed_url: @feed.feed
-        #   },
-        #   rss: {
-        #     feed_title: "RSS",
-        #     feed_url: @feed.http
-        #   },
-        #   itunes: {
-        #     feed_title: "iTunes",
-        #     feed_url: @feed.itpc
-        #   },
-        #   apple_podcasts: {
-        #     feed_title: "Apple Podcasts",
-        #     feed_url: @feed.podcast
-        #   }
-        # },
-        # ios: {
-        #   default: {
-        #     feed_title: "Default",
-        #     feed_url: @feed.feed
-        #   }, 
-        #   apple_podcasts: {
-        #     feed_title: "Apple Podcasts",
-        #     feed_url: @feed.podcast
-        #   }, 
-        #   overcast: {
-        #     feed_title: "Overcast",
-        #     feed_url: @feed.overcast
-        #   }, 
-        #   castro: {
-        #     feed_title: "Castro",
-        #     feed_url: @feed.castro
-        #   }, 
-        #   pocket_casts: {
-        #     feed_title: "Pocket Casts",
-        #     feed_url: @feed.pktc
-        #   }, 
-        #   downcast: {
-        #     feed_title: "Downcast",
-        #     feed_url: @feed.downcast
-        #   }
-        # },
-        # android: {
-        #   default: {
-        #     feed_title: "Default",
-        #     feed_url: @feed.pcast           
-        #   }
-        # }
       }
     end
-    # rubocop:enable Metrics/MethodLength
 
     def profile_params
       params
