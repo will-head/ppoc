d953b0fc76d63193a7827c91c4c1e3ee0da3201c
@willhead <60652390+will-head@users.noreply.github.com>
2020-09-22 14:16:35 +0100

    Adds with valid request context

--- spec/lib/feed_translator_spec.rb
+++ spec/lib/feed_translator_spec.rb
@@ -4,189 +4,191 @@ require 'feed_translator'
 describe FeedTranslator do
   subject(:feed) { described_class.new(source_feed) }
 
-  let(:source_feed) { "http://example.com/feed" }
+  context "with valid request" do 
+    let(:source_feed) { "http://example.com/feed" }
 
-  describe "#body" do
-    it "includes :request key" do
-      expect(feed.body).to have_key(:request)
+    describe "#body" do
+      it "includes :request key" do
+        expect(feed.body).to have_key(:request)
+      end
+
+      it "includes :desktop key" do
+        expect(feed.body).to have_key(:desktop)
+      end
+
+      it "includes :ios key" do
+        expect(feed.body).to have_key(:ios)
+      end
+
+      it "includes :android key" do
+        expect(feed.body).to have_key(:android)
+      end
     end
 
-    it "includes :desktop key" do
-      expect(feed.body).to have_key(:desktop)
+    describe "#body[:request]" do
+      it "[:feed] = 'http://example.com/feed'" do 
+        expect(feed.body[:request][:feed]).to eq("http://example.com/feed")
+      end
     end
 
-    it "includes :ios key" do
-      expect(feed.body).to have_key(:ios)
+    describe "#body[:desktop]" do
+      it "includes :default key" do
+        expect(feed.body[:desktop]).to have_key(:default)
+      end
+
+      it "includes :rss key" do
+        expect(feed.body[:desktop]).to have_key(:rss)
+      end
+
+      it "includes :itunes key" do
+        expect(feed.body[:desktop]).to have_key(:itunes)
+      end
+
+      it "includes :apple_podcasts key" do
+        expect(feed.body[:desktop]).to have_key(:apple_podcasts)
+      end
     end
 
-    it "includes :android key" do
-      expect(feed.body).to have_key(:android)
-    end
-  end
+    describe "#body[:desktop][:default]" do
+      it "[:feed_title] = 'Default'" do
+        expect(feed.body[:desktop][:default][:feed_title]).to eq("Default")
+      end
 
-  describe "#body[:request]" do
-    it "[:feed] = 'http://example.com/feed'" do 
-      expect(feed.body[:request][:feed]).to eq("http://example.com/feed")
-    end
-  end
-
-  describe "#body[:desktop]" do
-    it "includes :default key" do
-      expect(feed.body[:desktop]).to have_key(:default)
+      it "[:feed_url] = 'feed://example.com/feed'" do
+        expect(feed.body[:desktop][:default][:feed_url]).to eq("feed://example.com/feed")
+      end
     end
 
-    it "includes :rss key" do
-      expect(feed.body[:desktop]).to have_key(:rss)
+    describe "#body[:desktop][:rss]" do
+      it "[:feed_title] = 'RSS'" do
+        expect(feed.body[:desktop][:rss][:feed_title]).to eq("RSS")
+      end
+
+      it "[:feed_url] = 'http://example.com/feed'" do
+        expect(feed.body[:desktop][:rss][:feed_url]).to eq("http://example.com/feed")
+      end
     end
 
-    it "includes :itunes key" do
-      expect(feed.body[:desktop]).to have_key(:itunes)
+    describe "#body[:desktop][:itunes]" do
+      it "[:feed_title] = 'iTunes'" do
+        expect(feed.body[:desktop][:itunes][:feed_title]).to eq("iTunes")
+      end
+
+      it "[:feed_url] = 'itpc://example.com/feed'" do
+        expect(feed.body[:desktop][:itunes][:feed_url]).to eq("itpc://example.com/feed")
+      end
     end
 
-    it "includes :apple_podcasts key" do
-      expect(feed.body[:desktop]).to have_key(:apple_podcasts)
-    end
-  end
+    describe "#body[:desktop][:apple_podcasts]" do
+      it "[:feed_title] = 'Apple Podcasts'" do
+        expect(feed.body[:desktop][:apple_podcasts][:feed_title]).to eq("Apple Podcasts")
+      end
 
-  describe "#body[:desktop][:default]" do
-    it "[:feed_title] = 'Default'" do
-      expect(feed.body[:desktop][:default][:feed_title]).to eq("Default")
-    end
-
-    it "[:feed_url] = 'feed://example.com/feed'" do
-      expect(feed.body[:desktop][:default][:feed_url]).to eq("feed://example.com/feed")
-    end
-  end
-
-  describe "#body[:desktop][:rss]" do
-    it "[:feed_title] = 'RSS'" do
-      expect(feed.body[:desktop][:rss][:feed_title]).to eq("RSS")
+      it "[:feed_url] = 'podcast://example.com/feed'" do
+        expect(feed.body[:desktop][:apple_podcasts][:feed_url]).to eq("podcast://example.com/feed")
+      end
     end
 
-    it "[:feed_url] = 'http://example.com/feed'" do
-      expect(feed.body[:desktop][:rss][:feed_url]).to eq("http://example.com/feed")
-    end
-  end
+    describe "#body[:ios]" do
+      it "includes :default key" do
+        expect(feed.body[:ios]).to have_key(:default)
+      end
 
-  describe "#body[:desktop][:itunes]" do
-    it "[:feed_title] = 'iTunes'" do
-      expect(feed.body[:desktop][:itunes][:feed_title]).to eq("iTunes")
-    end
+      it "includes :apple_podcasts key" do
+        expect(feed.body[:ios]).to have_key(:apple_podcasts)
+      end
 
-    it "[:feed_url] = 'itpc://example.com/feed'" do
-      expect(feed.body[:desktop][:itunes][:feed_url]).to eq("itpc://example.com/feed")
-    end
-  end
+      it "includes :overcast key" do
+        expect(feed.body[:ios]).to have_key(:overcast)
+      end
 
-  describe "#body[:desktop][:apple_podcasts]" do
-    it "[:feed_title] = 'Apple Podcasts'" do
-      expect(feed.body[:desktop][:apple_podcasts][:feed_title]).to eq("Apple Podcasts")
-    end
+      it "includes :castro key" do
+        expect(feed.body[:ios]).to have_key(:castro)
+      end
 
-    it "[:feed_url] = 'podcast://example.com/feed'" do
-      expect(feed.body[:desktop][:apple_podcasts][:feed_url]).to eq("podcast://example.com/feed")
-    end
-  end
+      it "includes :pocket_casts key" do
+        expect(feed.body[:ios]).to have_key(:pocket_casts)
+      end
 
-  describe "#body[:ios]" do
-    it "includes :default key" do
-      expect(feed.body[:ios]).to have_key(:default)
+      it "includes :downcast key" do
+        expect(feed.body[:ios]).to have_key(:downcast)
+      end
     end
 
-    it "includes :apple_podcasts key" do
-      expect(feed.body[:ios]).to have_key(:apple_podcasts)
+    describe "#body[:ios][:default]" do
+      it "[:feed_title] = 'Default'" do
+        expect(feed.body[:ios][:default][:feed_title]).to eq("Default")
+      end
+
+      it "[:feed_url] = 'feed://example.com/feed'" do
+        expect(feed.body[:ios][:default][:feed_url]).to eq("feed://example.com/feed")
+      end
     end
 
-    it "includes :overcast key" do
-      expect(feed.body[:ios]).to have_key(:overcast)
+    describe "#body[:ios][:apple_podcasts]" do
+      it "[:feed_title] = 'Apple Podcasts'" do
+        expect(feed.body[:ios][:apple_podcasts][:feed_title]).to eq("Apple Podcasts")
+      end
+
+      it "[:feed_url] = 'podcast://example.com/feed'" do
+        expect(feed.body[:ios][:apple_podcasts][:feed_url]).to eq("podcast://example.com/feed")
+      end
     end
 
-    it "includes :castro key" do
-      expect(feed.body[:ios]).to have_key(:castro)
+    describe "#body[:ios][:overcast]" do
+      it "[:feed_title] = 'Overcast'" do
+        expect(feed.body[:ios][:overcast][:feed_title]).to eq("Overcast")
+      end
+
+      it "[:feed_url] = 'overcast://x-callback-url/add?url=http%3A%2F%2Fexample.com%2Ffeed'" do
+        expect(feed.body[:ios][:overcast][:feed_url]).to eq("overcast://x-callback-url/add?url=http%3A%2F%2Fexample.com%2Ffeed")
+      end
     end
 
-    it "includes :pocket_casts key" do
-      expect(feed.body[:ios]).to have_key(:pocket_casts)
+    describe "#body[:ios][:castro]" do
+      it "[:feed_title] = 'Castro'" do
+        expect(feed.body[:ios][:castro][:feed_title]).to eq("Castro")
+      end
+
+      it "[:feed_url] = 'castro://subscribe/example.com/feed'" do
+        expect(feed.body[:ios][:castro][:feed_url]).to eq("castro://subscribe/example.com/feed")
+      end
     end
 
-    it "includes :downcast key" do
-      expect(feed.body[:ios]).to have_key(:downcast)
-    end
-  end
+    describe "#body[:ios][:pocket_casts]" do
+      it "[:feed_title] = 'Pocket Casts'" do
+        expect(feed.body[:ios][:pocket_casts][:feed_title]).to eq("Pocket Casts")
+      end
 
-  describe "#body[:ios][:default]" do
-    it "[:feed_title] = 'Default'" do
-      expect(feed.body[:ios][:default][:feed_title]).to eq("Default")
-    end
-
-    it "[:feed_url] = 'feed://example.com/feed'" do
-      expect(feed.body[:ios][:default][:feed_url]).to eq("feed://example.com/feed")
-    end
-  end
-
-  describe "#body[:ios][:apple_podcasts]" do
-    it "[:feed_title] = 'Apple Podcasts'" do
-      expect(feed.body[:ios][:apple_podcasts][:feed_title]).to eq("Apple Podcasts")
+      it "[:feed_url] = 'pktc://subscribe/example.com/feed'" do
+        expect(feed.body[:ios][:pocket_casts][:feed_url]).to eq("pktc://subscribe/example.com/feed")
+      end
     end
 
-    it "[:feed_url] = 'podcast://example.com/feed'" do
-      expect(feed.body[:ios][:apple_podcasts][:feed_url]).to eq("podcast://example.com/feed")
-    end
-  end
+    describe "#body[:ios][:downcast]" do
+      it "[:feed_title] = 'Downcast'" do
+        expect(feed.body[:ios][:downcast][:feed_title]).to eq("Downcast")
+      end
 
-  describe "#body[:ios][:overcast]" do
-    it "[:feed_title] = 'Overcast'" do
-      expect(feed.body[:ios][:overcast][:feed_title]).to eq("Overcast")
-    end
-
-    it "[:feed_url] = 'overcast://x-callback-url/add?url=http%3A%2F%2Fexample.com%2Ffeed'" do
-      expect(feed.body[:ios][:overcast][:feed_url]).to eq("overcast://x-callback-url/add?url=http%3A%2F%2Fexample.com%2Ffeed")
-    end
-  end
-
-  describe "#body[:ios][:castro]" do
-    it "[:feed_title] = 'Castro'" do
-      expect(feed.body[:ios][:castro][:feed_title]).to eq("Castro")
+      it "[:feed_url] = 'downcast://example.com/feed'" do
+        expect(feed.body[:ios][:downcast][:feed_url]).to eq("downcast://example.com/feed")
+      end
     end
 
-    it "[:feed_url] = 'castro://subscribe/example.com/feed'" do
-      expect(feed.body[:ios][:castro][:feed_url]).to eq("castro://subscribe/example.com/feed")
-    end
-  end
-
-  describe "#body[:ios][:pocket_casts]" do
-    it "[:feed_title] = 'Pocket Casts'" do
-      expect(feed.body[:ios][:pocket_casts][:feed_title]).to eq("Pocket Casts")
+    describe "#body[:android]" do
+      it "includes :default key" do
+        expect(feed.body[:android]).to have_key(:default)
+      end
     end
 
-    it "[:feed_url] = 'pktc://subscribe/example.com/feed'" do
-      expect(feed.body[:ios][:pocket_casts][:feed_url]).to eq("pktc://subscribe/example.com/feed")
-    end
-  end
+    describe "#body[:android][:default]" do
+      it "[:feed_title] = 'Default'" do
+        expect(feed.body[:android][:default][:feed_title]).to eq("Default")
+      end
 
-  describe "#body[:ios][:downcast]" do
-    it "[:feed_title] = 'Downcast'" do
-      expect(feed.body[:ios][:downcast][:feed_title]).to eq("Downcast")
-    end
-
-    it "[:feed_url] = 'downcast://example.com/feed'" do
-      expect(feed.body[:ios][:downcast][:feed_url]).to eq("downcast://example.com/feed")
-    end
-  end
-
-  describe "#body[:android]" do
-    it "includes :default key" do
-      expect(feed.body[:android]).to have_key(:default)
-    end
-  end
-
-  describe "#body[:android][:default]" do
-    it "[:feed_title] = 'Default'" do
-      expect(feed.body[:android][:default][:feed_title]).to eq("Default")
-    end
-
-    it "[:feed_url] = 'pcast://example.com/feed'" do
-      expect(feed.body[:android][:default][:feed_url]).to eq("pcast://example.com/feed")
+      it "[:feed_url] = 'pcast://example.com/feed'" do
+        expect(feed.body[:android][:default][:feed_url]).to eq("pcast://example.com/feed")
+      end
     end
   end
 end
