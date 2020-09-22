14ed3a11e851fffad1cc90c0fb8653cad75e756f
@willhead <60652390+will-head@users.noreply.github.com>
2020-09-22 12:40:02 +0100

    Refactors to use FeedTranslator desktop, iso and android methods

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -13,59 +13,60 @@ module Api::V1
     # rubocop:disable Metrics/MethodLength
     def body
       {
-        request: {
-          feed:  profile_params.fetch(:feed)
-        },
-        desktop: {
-          default: {
-            feed_title: "Default",
-            feed_url: @feed.feed
-          },
-          rss: {
-            feed_title: "RSS",
-            feed_url: @feed.http
-          },
-          itunes: {
-            feed_title: "iTunes",
-            feed_url: @feed.itpc
-          },
-          apple_podcasts: {
-            feed_title: "Apple Podcasts",
-            feed_url: @feed.podcast
-          }
-        },
-        ios: {
-          default: {
-            feed_title: "Default",
-            feed_url: @feed.feed
-          }, 
-          apple_podcasts: {
-            feed_title: "Apple Podcasts",
-            feed_url: @feed.podcast
-          }, 
-          overcast: {
-            feed_title: "Overcast",
-            feed_url: @feed.overcast
-          }, 
-          castro: {
-            feed_title: "Castro",
-            feed_url: @feed.castro
-          }, 
-          pocket_casts: {
-            feed_title: "Pocket Casts",
-            feed_url: @feed.pktc
-          }, 
-          downcast: {
-            feed_title: "Downcast",
-            feed_url: @feed.downcast
-          }
-        },
-        android: {
-          default: {
-            feed_title: "Default",
-            feed_url: @feed.pcast           
-          }
-        }
+        request: { feed: profile_params.fetch(:feed) },
+        desktop: @feed.desktop,
+        ios: @feed.ios,
+        android: @feed.android
+        # desktop: {
+        #   default: {
+        #     feed_title: "Default",
+        #     feed_url: @feed.feed
+        #   },
+        #   rss: {
+        #     feed_title: "RSS",
+        #     feed_url: @feed.http
+        #   },
+        #   itunes: {
+        #     feed_title: "iTunes",
+        #     feed_url: @feed.itpc
+        #   },
+        #   apple_podcasts: {
+        #     feed_title: "Apple Podcasts",
+        #     feed_url: @feed.podcast
+        #   }
+        # },
+        # ios: {
+        #   default: {
+        #     feed_title: "Default",
+        #     feed_url: @feed.feed
+        #   }, 
+        #   apple_podcasts: {
+        #     feed_title: "Apple Podcasts",
+        #     feed_url: @feed.podcast
+        #   }, 
+        #   overcast: {
+        #     feed_title: "Overcast",
+        #     feed_url: @feed.overcast
+        #   }, 
+        #   castro: {
+        #     feed_title: "Castro",
+        #     feed_url: @feed.castro
+        #   }, 
+        #   pocket_casts: {
+        #     feed_title: "Pocket Casts",
+        #     feed_url: @feed.pktc
+        #   }, 
+        #   downcast: {
+        #     feed_title: "Downcast",
+        #     feed_url: @feed.downcast
+        #   }
+        # },
+        # android: {
+        #   default: {
+        #     feed_title: "Default",
+        #     feed_url: @feed.pcast           
+        #   }
+        # }
       }
     end
     # rubocop:enable Metrics/MethodLength
