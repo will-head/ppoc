4e6867e8c9672536827372274a240a4d1fcd4d6b
@willhead <60652390+will-head@users.noreply.github.com>
2020-10-12 14:42:46 +0100

    Adds test and basic implementation for #body request key

--- lib/feed_formatter.rb
+++ lib/feed_formatter.rb
@@ -1,3 +1,12 @@
 class FeedFormatter
 
+  def initialize(translated_feed)
+  end
+
+  def body
+    {
+      request: nil
+    }
+  end
+
 end
--- spec/lib/feed_formatter_spec.rb
+++ spec/lib/feed_formatter_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 require 'feed_formatter'
+require 'feed_translator'
 
 describe FeedFormatter do
-  
+
+  context "with http://example.com/feed" do 
+    let(:source_feed) { "http://example.com/feed" }
+    describe "#body" do
+      it "includes :request key" do
+        translated_feed = FeedTranslator.new(source_feed)
+        formatted_feed = FeedFormatter.new(translated_feed)
+        expect(formatted_feed.body).to have_key(:request)
+      end
+    end
+  end
 end
