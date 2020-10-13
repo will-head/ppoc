556454dd244a3c6f7ea7ba10b2ab491df22720b2
@willhead <60652390+will-head@users.noreply.github.com>
2020-10-13 15:31:36 +0100

    Moves tests for different input types to FeedTranslator

--- spec/controllers/api/v1/feeds_controller_spec.rb
+++ spec/controllers/api/v1/feeds_controller_spec.rb
@@ -10,69 +10,69 @@ RSpec.describe Api::V1::FeedsController, type: :controller do
       post :create, :params => request_body
     end
 
-    context "with basic feed" do
-      let(:source_feed) { "http://example.com/feed" }
+    # context "with basic feed" do
+    #   let(:source_feed) { "http://example.com/feed" }
 
-      it "returns itpc://example.com/feed" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed")
-      end
-    end
+    #   it "returns itpc://example.com/feed" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed")
+    #   end
+    # end
 
-    context "with trailing slash feed" do
-      let(:source_feed) { "http://example.com/feed/" }
+    # context "with trailing slash feed" do
+    #   let(:source_feed) { "http://example.com/feed/" }
 
-      it "returns itpc://example.com/feed/" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed/")
-      end
-    end
+    #   it "returns itpc://example.com/feed/" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed/")
+    #   end
+    # end
 
-    context "with secure feed" do
-      let(:source_feed) { "https://example.com/feed" }
+    # context "with secure feed" do
+    #   let(:source_feed) { "https://example.com/feed" }
 
-      it "returns itpc://example.com/feed" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed")
-      end
-    end
+    #   it "returns itpc://example.com/feed" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed")
+    #   end
+    # end
 
-    context "with parameter in feed" do
-      let(:source_feed) { "http://example.com/feed?rss" }
+    # context "with parameter in feed" do
+    #   let(:source_feed) { "http://example.com/feed?rss" }
 
-      it "returns itpc://example.com/feed?rss" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed?rss")
-      end
-    end
+    #   it "returns itpc://example.com/feed?rss" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed?rss")
+    #   end
+    # end
 
-    context "with parameter equals in feed" do
-      let(:source_feed) { "http://example.com/feed?format=rss" }
+    # context "with parameter equals in feed" do
+    #   let(:source_feed) { "http://example.com/feed?format=rss" }
 
-      it "returns itpc://example.com/feed?format=rss" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed?format=rss")
-      end
-    end
+    #   it "returns itpc://example.com/feed?format=rss" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed?format=rss")
+    #   end
+    # end
 
-    context "with extension in feed" do
-      let(:source_feed) { "http://example.com/feed.rss" }
+    # context "with extension in feed" do
+    #   let(:source_feed) { "http://example.com/feed.rss" }
 
-      it "returns itpc://example.com/feed.rss" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed.rss")
-      end
-    end
+    #   it "returns itpc://example.com/feed.rss" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://example.com/feed.rss")
+    #   end
+    # end
 
-    context "with username in feed" do
-      let(:source_feed) { "http://user@example.com/feed" }
+    # context "with username in feed" do
+    #   let(:source_feed) { "http://user@example.com/feed" }
 
-      it "returns itpc://user@example.com/feed" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://user@example.com/feed")
-      end
-    end
+    #   it "returns itpc://user@example.com/feed" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://user@example.com/feed")
+    #   end
+    # end
 
-    context "with username and password in feed" do
-      let(:source_feed) { "http://user:password@example.com/feed" }
+    # context "with username and password in feed" do
+    #   let(:source_feed) { "http://user:password@example.com/feed" }
 
-      it "returns itpc://user:password@example.com/feed" do
-        expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://user:password@example.com/feed")
-      end
-    end
+    #   it "returns itpc://user:password@example.com/feed" do
+    #     expect(json_body["desktop"]["itunes"]["feed_url"]).to eq("itpc://user:password@example.com/feed")
+    #   end
+    # end
 
     context "with different players" do
       let(:source_feed) { "http://example.com/feed" }
--- spec/lib/feed_translator_spec.rb
+++ spec/lib/feed_translator_spec.rb
@@ -4,8 +4,74 @@ require 'feed_translator'
 describe FeedTranslator do
   subject(:translated_feed) { described_class.new(source_feed) }
 
-  context "with valid input" do 
-    describe "http scheme" do 
+  context "with different types of valid input" do
+    describe "no trailing slash" do
+      let(:source_feed) { "http://example.com/feed" }
+
+      it "returns itpc://example.com/feed" do
+        expect(translated_feed.itpc).to eq("itpc://example.com/feed")
+      end
+    end
+
+    describe "with trailing slash feed" do
+      let(:source_feed) { "http://example.com/feed/" }
+
+      it "returns itpc://example.com/feed/" do
+        expect(translated_feed.itpc).to eq("itpc://example.com/feed/")
+      end
+    end
+
+    describe "with secure feed" do
+      let(:source_feed) { "https://example.com/feed" }
+
+      it "returns itpc://example.com/feed" do
+        expect(translated_feed.itpc).to eq("itpc://example.com/feed")
+      end
+    end
+
+    describe "with parameter in feed" do
+      let(:source_feed) { "http://example.com/feed?rss" }
+
+      it "returns itpc://example.com/feed?rss" do
+        expect(translated_feed.itpc).to eq("itpc://example.com/feed?rss")
+      end
+    end
+
+    describe "with parameter equals in feed" do
+      let(:source_feed) { "http://example.com/feed?format=rss" }
+
+      it "returns itpc://example.com/feed?format=rss" do
+        expect(translated_feed.itpc).to eq("itpc://example.com/feed?format=rss")
+      end
+    end
+
+    describe "with extension in feed" do
+      let(:source_feed) { "http://example.com/feed.rss" }
+
+      it "returns itpc://example.com/feed.rss" do
+        expect(translated_feed.itpc).to eq("itpc://example.com/feed.rss")
+      end
+    end
+
+    describe "with username in feed" do
+      let(:source_feed) { "http://user@example.com/feed" }
+
+      it "returns itpc://user@example.com/feed" do
+        expect(translated_feed.itpc).to eq("itpc://user@example.com/feed")
+      end
+    end
+
+    describe "with username and password in feed" do
+      let(:source_feed) { "http://user:password@example.com/feed" }
+
+      it "returns itpc://user:password@example.com/feed" do
+        expect(translated_feed.itpc).to eq("itpc://user:password@example.com/feed")
+      end
+    end
+  end
+
+  context "with different valid input schemes" do 
+    describe "http" do 
       let(:source_feed) { "http://example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -25,7 +91,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "https scheme" do 
+    describe "https" do 
       let(:source_feed) { "https://example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -45,7 +111,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "feed scheme" do 
+    describe "feed" do 
       let(:source_feed) { "feed://example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -66,7 +132,7 @@ describe FeedTranslator do
 
     end
 
-    describe "itpc scheme" do 
+    describe "itpc" do 
       let(:source_feed) { "itpc://example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -86,7 +152,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "podcast scheme" do 
+    describe "podcast" do 
       let(:source_feed) { "podcast://example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -106,7 +172,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "overcast scheme" do 
+    describe "overcast" do 
       let(:source_feed) { "overcast://x-callback-url/add?url=http%3A%2F%2Fexample.com%2Ffeed" }
 
       it "valid_request? doesn't return false" do 
@@ -126,7 +192,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "castro scheme" do 
+    describe "castro" do 
       let(:source_feed) { "castro://subscribe/example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -146,7 +212,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "pktc scheme" do 
+    describe "pktc" do 
       let(:source_feed) { "pktc://subscribe/example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -166,7 +232,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "downcast scheme" do 
+    describe "downcast" do 
       let(:source_feed) { "downcast://example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -186,7 +252,7 @@ describe FeedTranslator do
       end
     end
 
-    describe "pcast scheme" do 
+    describe "pcast" do 
       let(:source_feed) { "pcast://example.com/feed" }
 
       it "valid_request? doesn't return false" do 
@@ -205,7 +271,6 @@ describe FeedTranslator do
         expect(translated_feed.castro).to eq("castro://subscribe/example.com/feed")
       end
     end
-
   end
 
   context "with invalid input" do 
